9932fead7d516faa0323bfd16c3d6e6dfb4273da
Adverity Showcase <104839668+AdverityShowcase@users.noreply.github.com>
2026-04-13 11:18:02 +0100

    Update fct_finance_performance.sql

--- models/marts/fct_finance_performance.sql
+++ models/marts/fct_finance_performance.sql
@@ -1,8 +1,20 @@
 with combined as (
-    select * from {{ ref('stg_fb_ads') }}
-    union all
-    select * from {{ ref('stg_google_ads') }}
+    {% if var('selected_platform') == 'facebook' %}
+        -- Adverity told me to only run Facebook
+        select * from {{ ref('stg_fb_ads') }}
+
+    {% elif var('selected_platform') == 'google' %}
+        -- Adverity told me to only run Google
+        select * from {{ ref('stg_google_ads') }}
+
+    {% else %}
+        -- Default: Run both (the 'all' option)
+        select * from {{ ref('stg_fb_ads') }}
+        union all
+        select * from {{ ref('stg_google_ads') }}
+    {% endif %}
 )
+
 select
     ad_date,
     platform,
